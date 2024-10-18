d24933e8c0c4ab8997d36b8a85061fb18f6c758d
Rishi-Pardeshi <rishipardeshi567+github@gmail.com>
2024-10-18 17:53:40 +0530

    basics

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000", // localhost
+        "foobar-3000.app.github.dev", // Codespaces
+      ],
+    },
+  },
+};
 
 export default nextConfig;
